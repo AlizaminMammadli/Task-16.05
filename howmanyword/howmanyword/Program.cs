e9872889735977ab28abb76e7a28fe77b45b4443
Alizamin23 <alizaminam@code.edu.az>
2024-05-16 04:00:23 +0400

    Task 16.05 added

---
+++ howmanyword/howmanyword/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace howmanyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string smpWord = "Code Academy";
+           
+            char[] characters = new char[smpWord.Length];
+
+            for (int i = 0; i < smpWord.Length; i++)
+            {
+                characters[i] = smpWord[i];
+            }
+
+            int[] charactersCounts = new int[52];
+
+            foreach (char c in characters)
+            {
+                if (c != ' ')
+                {
+                    int index = char.ToUpper(c) - 'A';
+                    charactersCounts[index]++;
+                }
+            }
+            for (int i = 0; i < charactersCounts.Length; i++)
+            {
+                char currentChar = (char)('A' + i);
+                int count = charactersCounts[i];
+
+                if (count > 0)
+                {
+                    Console.WriteLine($"{currentChar} : {count}");
+                }
+            }
+        
+        }
+    }
+}
---
+++ odd number/odd number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+
+namespace odd_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10; 
+            int number2 = 20;
+            int oddCount = 0;
+
+            if (number1 > number2)
+            {
+                int temp = number1;
+                number1 = number2;
+                number2 = temp;
+            }
+
+            for (int i = number1; i <= number2; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    oddCount++;
+                }
+            }
+            Console.WriteLine($"Count of odd numbers between {number1} and {number2}: {oddCount}");
+        }
+    }
+}
